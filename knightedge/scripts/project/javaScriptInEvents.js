2f2e0b40735de9c6eb43704b2dd1b277cf0f502a
brandon-sato <brandon.sato@grupointegrado.br>
2026-04-14 16:53:33 -0300

    adicionado versão optimizada do jogo

--- knightedge/scripts/project/javaScriptInEvents.js
+++ knightedge/scripts/project/javaScriptInEvents.js
@@ -2,6 +2,17 @@
 
 const scriptsInEvents = {
 
+	async Folhagameover_Event2_Act6(runtime, localVars)
+	{
+		let faseatual = runtime.globalVars.fase;
+		runtime.goToLayout(faseatual);
+	},
+
+	async Folhagameover_Event3_Act6(runtime, localVars)
+	{
+		runtime.goToLayout("titlescreen")
+	},
+
 	async Folhamenuprincipal_Event1_Act5(runtime, localVars)
 	{
 		const proxlayout = runtime.layout.index + 1;
@@ -14,28 +25,17 @@ const scriptsInEvents = {
 		runtime.goToLayout(proxlayout);
 	},
 
-	async Folhagameover_Event2_Act6(runtime, localVars)
-	{
-		let faseatual = runtime.globalVars.fase;
-		runtime.goToLayout(faseatual);
-	},
-
-	async Folhagameover_Event3_Act6(runtime, localVars)
-	{
-		runtime.goToLayout("titlescreen")
-	},
-
-	async Folhagameplay_Event20_Act15(runtime, localVars)
+	async Folhagameplay_Event18_Act15(runtime, localVars)
 	{
 		runtime.goToLayout("gameoverscreen")
 	},
 
-	async Folhagameplay_Event32_Act7(runtime, localVars)
+	async Folhagameplay_Event29_Act7(runtime, localVars)
 	{
 		runtime.goToLayout("gameoverscreen");
 	},
 
-	async Folhagameplay_Event44_Act12(runtime, localVars)
+	async Folhagameplay_Event41_Act12(runtime, localVars)
 	{
 		const proxlayout = runtime.layout.index + 1;
 		runtime.goToLayout(proxlayout);
